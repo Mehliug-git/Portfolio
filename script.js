8b72cc58725819b7607f9d519572099b1c98b69e
Mehliug <78538454+guilhemguilhem@users.noreply.github.com>
2022-11-08 13:16:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//url parameter 
-if (window.location.search.indexOf('track=yes') > -1) {
-    alert('track present');
-} else {
-    alert('track not here');
-};
-
 
 let eventHub = new Vue({
   data: {
