ddec7d2b55dbbadf94e525fc4d13978e954cd1bd
Mehliug <guilhemferreira6@gmail.com>
2022-11-09 17:16:15 +0100

    UPDATE

--- terminal.js
+++ terminal.js
@@ -72,16 +72,16 @@ function dragElement(elmnt) {
   }
 }
 
-/*document.getElementsByClassName("terminal_input")[document.getElementsByClassName("terminal_input").length-1].onblur = function (event) { 
+document.getElementsByClassName("terminal_input")[document.getElementsByClassName("terminal_input").length-1] = function (event) { 
         var blurEl = this; 
         setTimeout(function() {
             blurEl.focus()
         }, 10);
     };
-*/
+
 
 function openTerminal() {
-    document.getElementById("destop").innerHTML = '<div class="terminal_wrapper"> <section id="terminal"> <nav class="terminal_nav"> <div class="terminal_nav__btns"> <button class="terminal_nav__btn t-exit">&#10005;</button> <button class="terminal_nav__btn">&#9723;</button> <button class="terminal_nav__btn">&#9472;</button> </div><p class="terminal_nav__title">Terminal (You can drag me)</p></nav> <section id="terminal_body"> <div class="terminal__text">Type anything hit Enter / Type clear to clear screen</div><div class="terminal_main"> <div class="terminal_input_left"> <span class="user_msg">sandy@root:</span><span class="user_loc">~</span><span class="user_doll">$</span> </div><input class="terminal_input" type="text"> </div></section> </section></div>'
+    document.getElementById("desktop").innerHTML = '<div class="terminal_wrapper"> <section id="terminal"> <nav class="terminal_nav"> <div class="terminal_nav__btns"> <button class="terminal_nav__btn t-exit">&#10005;</button> <button class="terminal_nav__btn">&#9723;</button> <button class="terminal_nav__btn">&#9472;</button> </div><p class="terminal_nav__title">Terminal (You can drag me)</p></nav> <section id="terminal_body"> <div class="terminal__text">Type anything hit Enter / Type clear to clear screen</div><div class="terminal_main"> <div class="terminal_input_left"> <span class="user_msg">sandy@root:</span><span class="user_loc">~</span><span class="user_doll">$</span> </div><input class="terminal_input" type="text"> </div></section> </section></div>'
 };
 
 createEvent();
